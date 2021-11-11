8cdc084df2163c2eda4a8f62a0eed7b205015c62
Amelia <amelia.klebba@gmail.com>
2021-11-11 07:25:54 -0600

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by amelia klebba on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
